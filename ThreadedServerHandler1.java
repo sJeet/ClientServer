e1757b1195eb37b7c340da74a58c74cb2d657015
sJeet <jeet.malay.shah@gmail.com>
2014-04-17 02:38:34 -0700

    Create ThreadedServerHandler1.java
    Threaded Server Handler by extending Threads.

---
+++ ThreadedServerHandler1.java
@@ -0,0 +1,53 @@
+package echoMultiThreadServer;
+
+import java.io.*;
+import java.net.*;
+
+class ThreadedServerHandler extends Thread { 
+    ThreadedServerHandler (Socket client) {
+      clientSocket = client;
+}
+
+    public void run() {
+    	try {
+    		BufferedReader in; 
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); 
+            PrintWriter out;
+            
+            out = new PrintWriter(new OutputStreamWriter(clientSocket.getOutputStream())); 
+
+            out.println("Write message over here.."); 
+            out.flush(); 
+
+            for (;;) {
+            	String str = in.readLine(); 
+                if (str == null) {
+                	break; 
+                }
+                else {
+                	out.println("Echo: " + str);  
+                    out.flush();
+                    System.out.println("Input [" + str+"]");
+                    
+                    if (str.trim().equals("QUIT")) {
+                            break;
+                    } 
+                    
+                }
+                  
+            }
+            
+    	}
+        
+    	catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    	
+        System.out.println("Multi Threaded Server stopped.");
+        
+    }
+    
+    private
+    Socket clientSocket;
+    
+}
